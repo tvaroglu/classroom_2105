c8658749dede0047f156e2964a2f12e3b085c430
Taylor Varoglu <tava8398@colorado.edu>
2021-05-12 15:22:50 -0600

    Add completed iterations 1-4

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,63 @@
 class Classroom
+  attr_reader :subject, :capacity
+  attr_accessor :students, :over_capacity, :wait_list
+
+  def initialize(subject='Study Hall', capacity=5)
+    @subject = subject.to_s
+    @capacity = capacity.to_s.to_i
+    @students = Array.new
+    @wait_list = Array.new
+    @over_capacity = false
+  end
+
+  def add_student(student)
+    if self.students.length < self.capacity
+      self.students << student
+    else
+      self.wait_list << student
+      self.over_capacity = true
+    end
+  end
+
+  def over_capacity?()
+    return self.over_capacity
+  end
+
+
+  def yell_at_students()
+    if self.students.length > 0
+      students = self.students.map do |student|
+        student.upcase
+      end
+    end
+
+    if self.wait_list.length > 0
+      wait_list = self.wait_list.map do |student|
+        student.upcase
+      end
+    end
+
+    if students && wait_list
+      return students.concat(wait_list)
+    elsif students
+      return students
+    end
+  end
+
+
+  def kick_out()
+    if over_capacity?() == true
+      self.students.shift
+
+      if self.wait_list.length > 0
+        self.students << self.wait_list.shift
+      end
+
+      if self.wait_list.length == 0
+        self.over_capacity = false
+      end
+    end
+  end
+
+
 end
--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -3,22 +3,22 @@ require './lib/classroom'
 
 describe Classroom do
   context 'Attributes' do
-    xit 'is a classroom' do
+    it 'is a classroom' do
       classroom = Classroom.new('History', 4)
       expect(classroom).to be_an_instance_of(Classroom)
     end
 
-    xit 'has a subject' do
+    it 'has a subject' do
       classroom = Classroom.new('History', 4)
       expect(classroom.subject).to eq('History')
     end
 
-    xit 'has a capacity' do
+    it 'has a capacity' do
       classroom = Classroom.new('History', 4)
       expect(classroom.capacity).to eq 4
     end
 
-    xit 'has no students by default' do
+    it 'has no students by default' do
       classroom = Classroom.new('History', 4)
       expect(classroom.students).to eq []
     end
@@ -27,7 +27,7 @@ describe Classroom do
   # Iteration 2
 
   context 'Students' do
-    xit 'returns a list of students' do
+    it 'returns a list of students' do
       classroom = Classroom.new('History', 4)
       classroom.add_student('Mike')
       classroom.add_student('Megan')
@@ -36,7 +36,7 @@ describe Classroom do
       expect(classroom.students).to eq ['Mike', 'Megan', 'Bob']
     end
 
-    xit 'returns a list of uppercased names' do
+    it 'returns a list of uppercased names' do
       classroom = Classroom.new('History', 4)
       classroom.add_student('Mike')
       classroom.add_student('Megan')
@@ -45,4 +45,63 @@ describe Classroom do
       expect(classroom.yell_at_students).to eq ['MIKE', 'MEGAN', 'BOB']
     end
   end
+
+  # Iterations 3 & 4
+
+  context 'Capacity and Wait List' do
+    it 'returns a list of students and a wait list' do
+      classroom = Classroom.new('Algebra', 5)
+      expect(classroom.over_capacity?).to be(false)
+
+      students = ['Kyle', 'Stan', 'Kenny', 'Eric', 'Craig', 'Butters']
+      students.each do |student|
+        classroom.add_student(student)
+      end
+
+      expect(classroom.over_capacity?).to be(true)
+      expect(classroom.students).to eq(['Kyle', 'Stan', 'Kenny', 'Eric', 'Craig'])
+      expect(classroom.wait_list).to eq(['Butters'])
+      expect(classroom.yell_at_students).to eq(['KYLE', 'STAN', 'KENNY', 'ERIC', 'CRAIG', 'BUTTERS'])
+    end
+
+    it 'can remove a student to reach capacity' do
+      classroom = Classroom.new('Biology', 6)
+      expect(classroom.over_capacity?).to be(false)
+
+      students = ['Kyle', 'Stan', 'Kenny', 'Eric', 'Craig', 'Butters', 'Wendy', 'Jason']
+      students.each do |student|
+        classroom.add_student(student)
+      end
+
+      expect(classroom.over_capacity?).to be(true)
+      expect(classroom.students.length).to eq(6)
+      expect(classroom.wait_list.length).to eq(2)
+
+      classroom.kick_out
+
+      expect(classroom.over_capacity?).to be(true)
+      expect(classroom.students.length).to eq(6)
+      expect(classroom.students.first).to eq('Stan')
+      expect(classroom.students.last).to eq('Wendy')
+      expect(classroom.wait_list.length).to eq(1)
+
+      classroom.kick_out
+
+      expect(classroom.over_capacity?).to be(false)
+      expect(classroom.students.length).to eq(6)
+      expect(classroom.students.first).to eq('Kenny')
+      expect(classroom.students.last).to eq('Jason')
+      expect(classroom.wait_list.length).to eq(0)
+
+      classroom.kick_out
+
+      expect(classroom.over_capacity?).to be(false)
+      expect(classroom.students.length).to eq(6)
+      expect(classroom.students.first).to eq('Kenny')
+      expect(classroom.students.last).to eq('Jason')
+      expect(classroom.wait_list.length).to eq(0)
+    end
+  end
+
+
 end
